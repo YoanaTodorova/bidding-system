42ddcab3e96bc8ac020b1ccbdb0e9c8b65617024
YoanaTodorova <todorova.ioana@gmail.com>
2014-04-21 11:04:59 +0300

    add restrictions to comment leaving

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find params[:id]
   end
+
+  private
+
+  def can_comment_on_user? user
+    session[:user_id] != user[:id] if session[:user_id]
+  end
+
+  helper_method :can_comment_on_user?
 end
